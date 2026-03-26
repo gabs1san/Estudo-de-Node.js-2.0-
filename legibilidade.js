a45f473eac0bb62dc62e2b1fde963aa8a881157a
gabs1san <sanbiel398@gmail.com>
2026-03-26 15:06:35 -0300

    testando

---
+++ legibilidade.js
@@ -0,0 +1,12 @@
+let altura = 1.70
+// Ao ler de primeria já se sabe o que é, se a variável fosse apenas 'x' seria maios difídicl 
+console.log(altura)
+
+// Letras maiusculas e minusculas fazem diferença no Js
+let preco = 15
+let Preco = 20
+
+console.log('preco: ', preco)
+console.log('Preco: ', Preco)
+// preO não funciona 
+
---
+++ pratica.js
@@ -0,0 +1,41 @@
+// Exercício 1
+let nome = 'Gabriel Almeida San Gregorio'
+console.log('Olá!! Meu nome é: ', nome)
+
+// Exercício 2
+let anoAtual = 2026
+let anoDeNascimento = 2003
+
+const idade = anoAtual - anoDeNascimento
+console.log('Olá! você tem: ',idade,'anos')
+
+// Exercício 3
+let cidade = 'Caieiras'
+let estado = 'São Paulo'
+let nacao = 'Brazil'
+
+console.log('Você está em:', cidade, estado, nacao)
+
+// Exercício 4
+let saldo = 0
+console.log('Seu saldo inicial é:', saldo)
+
+let deposito = 200
+let soma = saldo + deposito 
+console.log('Após seu deposíto o valor é de:', soma)
+
+let saque = 50
+let subtracao = soma - saque
+console.log('Após o saque, o valor em sua conta é de:', subtracao)
+
+// exercício 5
+let matematica = 7.5
+let portugues = 8.5
+let ciencias = 9
+
+let somaDasNotas = matematica + portugues + ciencias 
+let media = somaDasNotas/3
+
+console.log('A sua média nesse bimestre é de:', media)
+
+// Exercício 6
