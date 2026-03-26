b4b0cf6f15ceeb76a365185412463ce8cefed6fa
gabs1san <sanbiel398@gmail.com>
2026-03-26 14:26:29 -0300

    Continuando os estudos, aprendendo sobre delcaração

---
+++ hoisting.js
@@ -0,0 +1,5 @@
+// console.log('Minha idade atual é:', idade)
+
+let idade = 22
+// Variável global não pode ser refernciada antes da declaração, o var pode e o let e o const não podem. 
+console.log('Minha idade atual depois de atribuir: ', idade)
--- script.js
+++ script.js
@@ -1,7 +1,13 @@
+// 1- Variável global como por exemplo "idade"
+// 2 - Forma antiga usando o var
+// 3 - Atualmente usando o let 
+// 4 - const de constante = um valor que não será modificado 
+
+
 idade = 22
 
 console.log('minha idade atual é:', idade)
 
 idade = 23
 
-console.log('minha idade atual depois do aniversário é:', idade)
+console.log('minha idade atual depois do aniversário é:', idade) 
