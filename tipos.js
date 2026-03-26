7ec6a80a9a8c16c7f9b3f90a642997f370197089
gabs1san <sanbiel398@gmail.com>
2026-03-26 14:39:42 -0300

    tipos

---
+++ tipos.js
@@ -0,0 +1,36 @@
+let nome = 'Gabriel'
+let idade = 21
+let altura = 1.68
+
+
+// diz o tipo da vairável 
+console.log(typeof nome) 
+console.log(typeof idade) 
+console.log(typeof altura) 
+
+// Boolean 
+let estaChovendo = false 
+console.log(typeof estaChovendo)
+
+let cidade 
+console.log(typeof cidade)
+
+// Nulo
+let jaAprendeuJs = null
+console.log(typeof jaAprendeuJs)
+
+//  Valor nulo, declaração de objeto 
+// Udefined não tem definição, pode ser nulo ou pode ter valor
+
+let simbulo = Symbol()
+console.log(typeof simbulo)
+
+// possíveis tipos:
+
+// string
+// number
+// number
+// boolean
+// undefined
+// object
+// symbol
