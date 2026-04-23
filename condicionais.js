203c73a5b961e7e27fb1dc1fd639729e095b9591
gabs1san <sanbiel398@gmail.com>
2026-04-23 18:27:07 -0300

    continuando os estudos

---
+++ condicionais.js
@@ -0,0 +1,22 @@
+const idade = 17
+
+
+if(idade >= 18) {
+    console.log("É maior de idade")
+}
+else{
+    console.log("É menor de idade: ")
+}
+
+
+const notaDoAluno = 3
+
+if(notaDoAluno >= 9) {
+    console.log("A nota do aluno foi excelente!!!")
+} else if (notaDoAluno >= 7) {
+    console.log("A nota do aluno foi boa")
+} else if (notaDoAluno >= 4) {
+    console.log("A nota do alno foi mediana")
+} else {
+    console.log("A nota do aluno foi péssima!")
+}
