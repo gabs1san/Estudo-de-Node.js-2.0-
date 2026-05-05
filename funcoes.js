75b8ba2f3b50f631842bc19d99f7f41fc2eb2e2d
gabs1san <sanbiel398@gmail.com>
2026-05-05 18:51:35 -0300

    Arrow Functions

--- funcoes.js
+++ funcoes.js
@@ -1,10 +1,10 @@
 // nome é um parametro da função saudacao
-function sudacao(nome) {  
-    console.log('Olá,', nome)
-}
+// function sudacao(nome) {  
+//     console.log('Olá,', nome)
+// }
 
-// Argumento
-sudacao('Gabriel') 
+// // Argumento
+// sudacao('Gabriel') 
 
 // Parametro é a definição da função e quando executa essa função se passa o Argumento
 
@@ -14,3 +14,20 @@ function calcularDobro(numero) {
 
 const numDobrado = calcularDobro(4)
 console.log('O dobro de 4 é:', numDobrado)
+
+// Sintaxe de Arrow Functions 
+const saudacao = (nome) => {
+    console.log('Via longa e prósprera,', nome)
+}      
+
+saudacao ('Gabriel')
+
+// funções simples
+const oi = nome => console.log('Olá,', nome)
+
+oi('Gabriel')
+
+const dobroNum = number => number * 2
+
+const numDobro = dobroNum(8)
+console.log('O dobro de 8 é:', numDobro)
