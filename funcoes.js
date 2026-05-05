671a7711742a02ae5802f170a9c2b5b11d60eb38
gabs1san <sanbiel398@gmail.com>
2026-05-05 18:32:27 -0300

    Funções

---
+++ funcoes.js
@@ -0,0 +1,16 @@
+// nome é um parametro da função saudacao
+function sudacao(nome) {  
+    console.log('Olá,', nome)
+}
+
+// Argumento
+sudacao('Gabriel') 
+
+// Parametro é a definição da função e quando executa essa função se passa o Argumento
+
+function calcularDobro(numero) {
+    return  numero * 2 
+}
+
+const numDobrado = calcularDobro(4)
+console.log('O dobro de 4 é:', numDobrado)
